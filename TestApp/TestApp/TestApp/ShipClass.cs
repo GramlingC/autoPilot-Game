35c3667f3108a677041842ad88d9adae0f54db24
Nick Francke <nickfrancke13@gmail.com>
2017-10-31 18:10:41 -0700

    Adds GameStateClass and ShipClass cs files to solution, and puts them in proper namespace

--- TestApp/TestApp/TestApp/ShipClass.cs
+++ TestApp/TestApp/TestApp/ShipClass.cs
@@ -1,69 +1,72 @@
 ﻿using System;
 
-public class Ship
+namespace TestApp
 {
-    private int hullIntegrity = 100;
-    private int fuel = 100;
-    private int lifesigns = 100;
-    private int empathyLevel = 0;
-
-    public Ship()
-	{
-	}
-
-    public Ship(int hullIntegrity, int fuel, int lifesigns, int empathyLevel)
+    public class Ship
     {
-        this.hullIntegrity = hullIntegrity;
-        this.fuel = fuel;
-        this.lifesigns = lifesigns;
-        this.empathyLevel = empathyLevel;
-    }
+        private int hullIntegrity = 100;
+        private int fuel = 100;
+        private int lifesigns = 100;
+        private int empathyLevel = 0;
 
-    //hullIntegrity methods
-    public int HullIntegrity
-    {
-        get { return hullIntegrity; }
-        set { hullIntegrity = value; }
-    }
+        public Ship()
+        {
+        }
 
-    public void ChangeHullIntegrity(int change)
-    {
-        hullIntegrity += change;
-    }
+        public Ship(int hullIntegrity, int fuel, int lifesigns, int empathyLevel)
+        {
+            this.hullIntegrity = hullIntegrity;
+            this.fuel = fuel;
+            this.lifesigns = lifesigns;
+            this.empathyLevel = empathyLevel;
+        }
 
-    //fuel methods
-    public int Fuel
-    {
-        get { return fuel; }
-        set { fuel = value; }
-    }
+        //hullIntegrity methods
+        public int HullIntegrity
+        {
+            get { return hullIntegrity; }
+            set { hullIntegrity = value; }
+        }
 
-    public void ChangeFuel(int change)
-    {
-        fuel += change;
-    }
+        public void ChangeHullIntegrity(int change)
+        {
+            hullIntegrity += change;
+        }
 
-    //lifesigns methods
-    public int Lifesigns
-    {
-        get { return lifesigns; }
-        set { lifesigns = value; }
-    }
+        //fuel methods
+        public int Fuel
+        {
+            get { return fuel; }
+            set { fuel = value; }
+        }
 
-    public void ChangeLifesigns(int change)
-    {
-        lifesigns += change;
-    }
+        public void ChangeFuel(int change)
+        {
+            fuel += change;
+        }
 
-    //empathyLevel methods
-    public int EmpathyLevel
-    {
-        get { return empathyLevel; }
-        set { empathyLevel = value; }
-    }
+        //lifesigns methods
+        public int Lifesigns
+        {
+            get { return lifesigns; }
+            set { lifesigns = value; }
+        }
 
-    public void ChangeEmpathyLevel(int change)
-    {
-        empathyLevel += change;
+        public void ChangeLifesigns(int change)
+        {
+            lifesigns += change;
+        }
+
+        //empathyLevel methods
+        public int EmpathyLevel
+        {
+            get { return empathyLevel; }
+            set { empathyLevel = value; }
+        }
+
+        public void ChangeEmpathyLevel(int change)
+        {
+            empathyLevel += change;
+        }
     }
 }
